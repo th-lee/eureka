50ee9d822719dcbe73585203613f25c3286c26b5
Taeho Lee <thlee@nextree.co.kr>
2018-04-29 15:47:27 +0900

    Delete run.sh

--- target/dockerfile/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "********************************************************"
-echo "Starting the Eureka Server"
-echo "********************************************************"
-java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/eurekaserver/eurekasvr-0.0.1-SNAPSHOT.jar
